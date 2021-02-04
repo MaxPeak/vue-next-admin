5e43e9433b07c3222f85b153067d76cc0c00e61e
张云峰 <zhangyf@dustess.com>
2021-02-04 09:13:31 +0800

    feat: 🎸 补充range util

--- src/utils/range.ts
+++ src/utils/range.ts
@@ -11,3 +11,20 @@ export const unSelect = (ele: HTMLElement, enable: boolean) => {
 export const clearSelections = () => {
   window.getSelection()?.removeAllRanges();
 };
+
+/**
+ * 选中全部文字
+ */
+export const selectAllRange = (ele: HTMLTextAreaElement) => {
+  if (ele.setSelectionRange) {
+    //说明是表单元素
+    ele.setSelectionRange(0, ele.value.length);
+  } else {
+    //是contenteditable="true"的可编辑元素
+    //创建range对象
+    const range = window.getSelection();
+    //选中ele元素下面的所有内容
+    range?.selectAllChildren(ele);
+  }
+  ele.focus();
+};
